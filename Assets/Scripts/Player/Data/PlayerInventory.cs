b0f1782fe702c23f2ef0a87afacf54779e516247
Danil Shulgin <nik.vasilevich.01@gmail.com>
2020-10-10 11:40:13 +0300

    add basic inventory API

--- Assets/Scripts/Player/Data/PlayerInventory.cs
+++ Assets/Scripts/Player/Data/PlayerInventory.cs
@@ -1,6 +1,54 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    public static event Action<IReadOnlyCollection<Item>> OnUpdate;
     
+    [SerializeField] private float maxNumber;
+    
+    [SerializeField] private List<Item> items;
+    
+    public float MaxNumber => maxNumber;
+    
+    public IReadOnlyCollection<Item> Items => items;
+
+    public static PlayerInventory Instance { get; private set; }
+
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;    
+        }
+        else
+        {
+            Debug.LogError("Multiple Player Inventory");
+        }
+    }
+
+
+    private void Start()
+    {
+        OnUpdate?.Invoke(items);
+    }
+
+    /// <returns>return false if full</returns>
+    public bool AddItem()
+    {
+        return true;
+    }
+    
+    public bool ContainsItem()
+    {
+        return true;
+    }
+    
+    /// <returns>return true if success</returns>
+    public bool RemoveItem()
+    {
+        return true;
+    }
 }
--- Assets/Scripts/UI/GameUI/InventoryUI.cs
+++ Assets/Scripts/UI/GameUI/InventoryUI.cs
@@ -1,6 +1,21 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    
+    private void Awake()
+    {
+        PlayerInventory.OnUpdate += HandleUpdateInventory;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerInventory.OnUpdate -= HandleUpdateInventory;
+    }
+
+    private void HandleUpdateInventory(IReadOnlyCollection<Item> inventoryItems)
+    {
+        
+    }
 }
