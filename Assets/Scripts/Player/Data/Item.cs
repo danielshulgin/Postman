5211971de09546c4b8585e72604dbbcaa0934ffb
Dan <kolisnychenko.dan2019@gmail.com>
2020-10-10 20:52:34 +0300

    aded inventory

--- Assets/Scripts/Player/Data/Item.cs
+++ Assets/Scripts/Player/Data/Item.cs
@@ -1,7 +1,12 @@
+using UnityEngine.UI;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Item", order = 1)]
 public class Item : ScriptableObject
 {
-    
+    public new string name;
+    public string description;
+    public Image image;
+    public bool isDropable;
+    public int amount;
 }
