6df047ff10b1ceb9d08d21a8b7aca7f890817179
andreymason <andreymason47@gmail.com>
2020-10-11 05:51:17 +0300

    Added Menu, added Scroll when we play, added Map on M-click

---
+++ Assets/Scenes/Map.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+	public Camera camera1;
+	public Camera camera2;
+	bool flag = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if( Input.GetKeyUp( KeyCode.M ) ) {
+			if(flag) {
+				camera1.enabled = false;
+				camera2.enabled = true;
+				flag = false;
+			}
+			else {
+				camera1.enabled = true;
+				camera2.enabled = false;
+				flag = true;
+			}
+			
+		}
+    }
+}
---
+++ Assets/Scenes/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void PlayPressed()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+	
+	public void ExitPressed()
+    {
+        Application.Quit();
+    }
+
+}
